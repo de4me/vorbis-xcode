9a796394265f63b21e5185a5897e89d89295203c
Denis Popov <aoh@list.ru>
2023-09-29 10:28:48 +0300

    Reconfigure project files;

---
+++ Xcode/vorbis/vorbis/vorbis.h
@@ -0,0 +1,20 @@
+//
+//  vorbis.h
+//  vorbis
+//
+//  Created by DE4ME on 29.09.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDL_gfx.
+FOUNDATION_EXPORT double vorbisVersionNumber;
+
+//! Project version string for SDL_gfx.
+FOUNDATION_EXPORT const unsigned char vorbisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDL_gfx/PublicHeader.h>
+
+#import <vorbis/vorbisfile.h>
+#import <vorbis/vorbisenc.h>
+
