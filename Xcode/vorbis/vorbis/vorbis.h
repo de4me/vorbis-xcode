8b8b49d04a2468bd18e8dccc83135910151cda57
Denis Popov <aoh@list.ru>
2023-09-29 11:40:35 +0300

    Improve code compatibility;

--- Xcode/vorbis/vorbis/vorbis.h
+++ Xcode/vorbis/vorbis/vorbis.h
@@ -5,16 +5,12 @@
 //  Created by DE4ME on 29.09.2023.
 //
 
-#import <Foundation/Foundation.h>
+#ifndef _vorbis_h_
+#define _vorbis_h_
 
-//! Project version number for SDL_gfx.
-FOUNDATION_EXPORT double vorbisVersionNumber;
 
-//! Project version string for SDL_gfx.
-FOUNDATION_EXPORT const unsigned char vorbisVersionString[];
+#include <vorbis/vorbisfile.h>
+#include <vorbis/vorbisenc.h>
 
-// In this header, you should import all the public headers of your framework using statements like #import <SDL_gfx/PublicHeader.h>
 
-#import <vorbis/vorbisfile.h>
-#import <vorbis/vorbisenc.h>
-
+#endif
